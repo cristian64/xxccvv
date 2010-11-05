f41031a7147348e87346a25c995cbfc44b358bf9
cyberjuen <cyberjuen@ca1f3eb6-eb7c-662b-8b76-c665defbf09f>
2010-11-05 23:16:22 +0000

    Brocilla modificada

--- ClickomaniaPath/AStarPathfinder.cpp
+++ ClickomaniaPath/AStarPathfinder.cpp
@@ -45,7 +45,7 @@ list< pair<Node*, int> > Node::childList() {
                 aux->y = currentY;
                 aux->map = this->map;
                 childs.push_back(pair<Node*, int>(aux, this->map->itemAt(currentX, currentY)->text().toInt()));
-                cout << "MIERDA " << currentX << ' ' << currentY << endl;
+                cout << "Generado vecino: " << currentX << ' ' << currentY << endl;
             }
         }
     }
