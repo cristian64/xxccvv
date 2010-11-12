585be2ed529d504fdb1bfba4ed3cab3d9b06d818
cristian64 <cristian64@ca1f3eb6-eb7c-662b-8b76-c665defbf09f>
2010-11-12 21:59:36 +0000

    Mejorado, supongo, los métodos que devuelven los grupos de movimientos de un tablero.

--- ClickomaniaGUI/Board.cpp
+++ ClickomaniaGUI/Board.cpp
@@ -170,30 +170,7 @@ void Board::gravity() {
 }
 
 list<set<std::pair<int, int> > > Board::getGroupMoves() const {
-    int elements = *this->rows * * this->columns;
-    bool *usedTiles = new bool [elements]; //marca que baldosas ya pertenecen a algun grupo
-    memset(usedTiles, false, sizeof (false) * elements);
-    list<set<std::pair<int, int> > > moves;
-    for (int i = 0; i<*this->rows; i++) {//para cada baldosa
-        for (int j = 0; j<*this->columns; j++) {
-            if (!usedTiles[i * (*this->columns) + j] && this->getPosition(j, i) != 0) {//si no esta en uso y no esta vacia
-                set<pair<int, int> > actualGroup;
-                usedTiles[i * (*this->columns) + j] = true; //Se marca como usada
-                actualGroup = this->getGroupMove(j, i); //preguntamos si esta en algun grupo
-                if (actualGroup.size() > 0) { //si hay un grupo ahi
-                    moves.push_back(actualGroup); //se añade a la lista de grupso
-                    set<std::pair<int, int> >::iterator it;
-                    for (it = moves.back().begin(); it != moves.back().end(); it++) {
-                        usedTiles[it->second * (*this->columns) + it->first] = true; //y se marcan todos sus integrantes como usados
-                    }
-                }
-            }
-        }
-    }
-
-    delete [] usedTiles;
-    return moves;
-
+	return getGrupos();
 }
 
 pair<int, int> Board::getMove(std::set<std::pair<int, int> > groupMove) const {
@@ -201,42 +178,7 @@ pair<int, int> Board::getMove(std::set<std::pair<int, int> > groupMove) const {
 }
 
 std::set<std::pair<int, int> > Board::getGroupMove(int x, int y) const {
-    std::list<std::pair<int, int> > open; //nodos candidatos
-    std::set<std::pair<int, int> > closed; //nodos procesados
-    set<std::pair<int, int> > connected; //grupo de vecinos
-    pair<int, int> actualTile; //
-    open.push_back(pair<int, int>(x, y)); //mete la baldosa pedida en abiertos
-    int xDir[] = {0, 0, -1, 1}; //4-vecindad cutre, algo pro estaria al pelo
-    int yDir[] = {1, -1, 0, 0};
-    int actualX, actualY;
-    if (this->getPosition(x, y) != 0) {//si la baldosa no esta vacia
-        while (!open.empty()) {//n //mientras haya candidatos
-            actualTile = open.front(); //coge el primero
-            open.pop_front(); //quitalo de abiertos
-            closed.insert(actualTile); //log//metelo en cerrados
-            connected.insert(actualTile); //log añadelo a la lista de vecinos
-            for (int dir = 0; dir < 4; dir++) {// 4 /para cada vecino
-                actualX = actualTile.first + xDir[dir];
-                actualY = actualTile.second + yDir[dir];
-                if (actualX >= 0 && actualX < *this->columns) {//si no se salen del tablero
-                    if (actualY >= 0 && actualY < *this->rows) {
-                        if (this->getPosition(actualX, actualY) == this->getPosition(x, y)) {//si tienen el mismo color
-                            pair<int, int> actual(actualX, actualY);
-                            if (closed.count(actual) == 0) { //log // si no ha sido procesado
-                                open.insert(open.end(), actual); //log // se mete en abiertos
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    // si hay solo un elemento en la lista de vecinos es que es una casilla aislada
-    // o esta vacia
-    if (connected.size() < 2) {
-        connected.clear();
-    }
-    return connected;
+	return getGrupo(y, x);
 }
 
 /*
--- ClickomaniaGUI/Board.h
+++ ClickomaniaGUI/Board.h
@@ -12,6 +12,10 @@
 #include <utility>
 #include <set>
 
+#include <string.h>
+
+using namespace std;
+
 class Board {
 public:
 
@@ -53,6 +57,106 @@ public:
 
     //calcula todos los movimientos(grupos) posibles dado un tablero
     std::list<std::set<std::pair<int, int> > > getGroupMoves() const;
+
+	/**
+	 * @brief
+	 *
+	 * @return Devuelve la lista de grupos que hay en el tablero.
+	*/
+	list<set<pair<int, int> > > getGrupos() const
+	{
+		list<set<pair<int, int> > > movimientos;
+		int total = *rows * *columns;
+		int *mascara = new int[total];
+		memset(mascara, 0, sizeof(int) * total);
+
+		// Se recorre el tablero. Cada casilla se procesa una vez (nos ayudamos de la máscara para ello).
+		for (int i = 0; i < total; i++)
+		{
+			// Si no se ha procesado, se procesa aplicando el algoritmo recursivo.
+			if (mascara[i] == 0 && board[i] != 0)
+			{
+				set<pair<int, int> > grupo;
+				algoritmo(i, mascara, grupo);
+				if (grupo.size() >= 2)
+					movimientos.push_back(grupo);
+			}
+		}
+
+		// Se libera la memoria y se devuelve el resultado.
+		delete [] mascara;
+		return movimientos;
+	}
+
+	/**
+	 * @brief Calcula el grupo de la casilla indicada y lo devuelve.
+	 *
+	 * @param fila Fila de la celda.
+	 * @param columna Columna de la celda.
+	 * @return Devuelve el grupo de la casilla indicada por la fila y la columna.
+	*/
+	set<pair<int, int> > getGrupo(int fila, int columna) const
+	{
+		set<pair<int, int> > grupo;
+
+		if (board[fila * *columns + columna] != 0)
+		{
+			int *mascara = new int[*rows * *columns];
+			memset(mascara, 0, sizeof(int) * *rows * *columns);
+
+			algoritmo(fila * *columns + columna, mascara, grupo);
+			if (grupo.size() < 2)
+				grupo.clear();
+			delete [] mascara;
+		}
+
+		return grupo;
+	}
+
+	/**
+	 * @brief Recorre el tablero recursivamente por inundación buscando las casillas del mismo color y las
+	 * introduce en un conjunto recibido por referencia en los parámetros.
+	 *
+	 * @param celda Celda que se va a comprobar.
+	 * @param mascara Vector que indica si la celda ha sido ocupada o no.
+	 * @param grupo Conjunto de celdas donde se coleccionarán las celdas. Tiene un valor por referencia.
+	*/
+	void algoritmo(int celda, int *mascara, set<pair<int, int> > &grupo) const
+	{
+		int fila = celda / *columns;
+		int columna = celda % *columns;
+
+		grupo.insert(pair<int, int>(columna, fila));
+		mascara[celda] = 1;
+
+		if (columna + 1 < *columns)
+		{
+			int izquierda = fila * *columns + (columna + 1);
+			if (mascara[izquierda] == 0 && board[celda] == board[izquierda])
+				algoritmo(izquierda, mascara, grupo);
+		}
+
+		if (columna - 1 >= 0)
+		{
+			int derecha = fila * *columns + (columna - 1);
+			if (mascara[derecha] == 0 && board[celda] == board[derecha])
+				algoritmo(derecha, mascara, grupo);
+		}
+
+		if (fila - 1 >= 0)
+		{
+			int arriba = (fila - 1) * *columns + columna;
+			if (mascara[arriba] == 0 && board[celda] == board[arriba])
+				algoritmo(arriba, mascara, grupo);
+		}
+
+		if (fila + 1 < *rows)
+		{
+			int abajo = (fila + 1) * *columns + columna;
+			if (mascara[abajo] == 0 && board[celda] == board[abajo])
+				algoritmo(abajo, mascara, grupo);
+		}
+	}
 
     /*
      * Devuelve la coordenada que define el movimiento (podria ser una cualquiera)
